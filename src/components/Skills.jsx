b464eac80554b555e657e1daa7a48bc080ec0e5b
Fouzia nazneen <155993032+fouzianazneen@users.noreply.github.com>
2024-10-10 09:56:16 +0000

    done skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import skills from "./data/skills.json";
 
 const Skills = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div className="container skills">
+        {/* <div className="container skills"></div> */}
+        <h1>SKILLS</h1>
+         <div className="items">
+        {skills.map((data) => (
+          <>
+            <div className="item" key={data.id}>
+              {/* <div className="item"> */}
+                <img src={`/assests/${data.imageSrc}`} alt="" />
+                <h3>{data.title}</h3>
+              </div>
+            {/* </div> */}
+          </>
+        ))}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Skills
+export default Skills;
