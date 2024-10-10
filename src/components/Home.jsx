c7f0ad5a53db1c946ed6f5029236e1c37c8d20e0
Fouzia nazneen <155993032+fouzianazneen@users.noreply.github.com>
2024-10-10 17:30:43 +0000

    done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,16 @@
+
+
+
+
+
 import React, { useEffect, useRef } from "react";
 import pdf from "../pdf/resume.pdf";
 import image from "./data/image.json";
 import Typed from "typed.js";
 
-
 const Home = () => {
   const typedRef = useRef(null);
+
   useEffect(() => {
     const options = {
       strings: [
@@ -25,12 +30,15 @@ const Home = () => {
       typed.destroy();
     };
   }, []);
+
   return (
     <>
       <div className="container home" id="home">
-        <div className="left"
-         data-aos="fade-up-right"
-         data-aos-duration="1000" >
+        <div
+          className="left"
+          data-aos="fade-up-right"
+          data-aos-duration="1000"
+        >
           <h1 ref={typedRef}></h1>
 
           <a
@@ -41,12 +49,14 @@ const Home = () => {
             Download Resume
           </a>
         </div>
-        <div className="right"
-         data-aos="fade-up-left"
-         data-aos-duration="1000" >
+        <div
+          className="right"
+          data-aos="fade-up-left"
+          data-aos-duration="1000"
+        >
           <div className="img">
-          <img src={`${process.env.PUBLIC_URL}/assets/image.png`} alt="image" />
-
+            {/* Corrected the path to /assets/ */}
+            <img src={`/assets/${image.imgSrc}`} alt="profile image" />
           </div>
         </div>
       </div>
