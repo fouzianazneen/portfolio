dffe6900a2f1d698933974116a3e548e636629ac
Fouzia nazneen <155993032+fouzianazneen@users.noreply.github.com>
2024-10-10 10:19:17 +0000

    half project done

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import project from "./data/projects.json";
 
 const Projects = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <div className="container projects my-3">
+        <h1>PROJECTS</h1>
+        <div className="row d-flex justify-content-center align-content-center">
+          {project.map((data) => (
+            <>
+              <div key={data.id} className="my-3 col-sm-6 col-md-4 col-lg-3 mx-3">
+                <div className="card bg-dark text-light" style={{ width: "18rem" }}>
+                  <img src={data.imageSrc} className="card-img-top" alt="..." />
+                  <div className="card-body text-center">
+                    <h5 className="card-title">{data.title}</h5>
+                    <p className="card-text">{data.description}</p>
+                    <a href="#" className="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Projects
+export default Projects;
