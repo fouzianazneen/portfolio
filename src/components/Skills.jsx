f01794eb23aa9179381800debb20d192de0ee158
Fouzia nazneen <155993032+fouzianazneen@users.noreply.github.com>
2024-10-10 22:31:56 +0530

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,37 +1,3 @@
-// import React from "react";
-// import skills from "./data/skills.json";
-
-// const Skills = () => {
-//   return (
-//     <>
-//       <div className="container skills" id="skills">
-//         {/* <div className="container skills"></div> */}
-//         <h1>SKILLS</h1>
-//          <div className="items"
-//           data-aos="flip-left"
-//           data-aos-duration="1000" >
-//         {skills.map((data) => (
-//           <>
-//             <div className="item"   key={data.id}>
-//               {/* <div className="item"> */}
-//                 <img src={`/assests/${data.imageSrc}`} alt="" />
-//                 <h3>{data.title}</h3>
-//               </div>
-//             {/* </div> */}
-//           </>
-//         ))}
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Skills;
-
-
-
-
-
 
 
 
